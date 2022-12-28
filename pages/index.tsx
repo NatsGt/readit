e4389153a2545c00bfcf7fce9a2852fc17b44d26
natsgt <nataliarodas7858@gmail.com>
2022-12-28 15:05:02 +0100

    redirect if authenticated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { useAuth } from '../hooks/AuthContext'
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
   const { values } = useAuth()
+  const router = useRouter()
   const handleSignIn = () => {
     values?.signIn()
   }
-  const handleSignOut = () => {
-    console.log('sign out', values?.isAuthenticated)
-    values?.signOut()
-  }
+
   console.log(values?.isAuthenticated)
+
+  useEffect(() => {
+    if (values?.isAuthenticated) {
+      router.push('/bookshelf')
+    }
+  }, [values])
 
   return (
     <div className={styles.container}>
@@ -26,7 +32,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>HELLO</h1>
         <button onClick={handleSignIn}>Sign in</button>
-        <button onClick={handleSignOut}>Sign out</button>
+
         <button
           onClick={() => console.log(values?.isAuthenticated, values?.user)}
         >
