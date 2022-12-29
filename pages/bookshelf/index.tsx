0fce814ede95e205acf820bb1073820c2d8ba2ad
natsgt <nataliarodas7858@gmail.com>
2022-12-29 22:18:45 +0100

    move fetch to results component, only search if query

--- pages/bookshelf/index.tsx
+++ pages/bookshelf/index.tsx
@@ -1,14 +1,15 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import SearchInput from '../../components/SearchInput'
+import SearchInput from '../../components/Search/SearchInput'
+import SearchResults from '../../components/Search/SearchResults'
 import { useAuth } from '../../hooks/AuthContext'
 import { useGetBooks } from '../../hooks/books'
 
 const Bookshelf = () => {
   const router = useRouter()
   const { isAuthenticated, signOut, user } = useAuth()
-  const [searchQuery, setSearchQuery] = useState()
-  const { data } = useGetBooks('isabel%20allende')
+  const [searchQuery, setSearchQuery] = useState<string>('')
+
   useEffect(() => {
     if (!isAuthenticated) {
       router.push('/')
@@ -18,13 +19,13 @@ const Bookshelf = () => {
     console.log('sign out', isAuthenticated)
     signOut()
   }
-  console.log(data)
 
   return (
     <div>
       Yeii {user.name}, you are in
       <button onClick={handleSignOut}>Sign out</button>
-      <SearchInput />
+      <SearchInput setSearchQuery={setSearchQuery} />
+      {searchQuery.length > 0 && <SearchResults query={searchQuery} />}
     </div>
   )
 }
