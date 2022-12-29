001927c5c5ee0e6e15dd0b870a530cd2620b0127
natsgt <nataliarodas7858@gmail.com>
2022-12-29 22:17:55 +0100

    add variable to query key

--- hooks/books.ts
+++ hooks/books.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
 export const useGetBooks = (query: string) => {
-  return useQuery(['books'], () =>
+  return useQuery(['books', query], () =>
     axios
       .get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
       .then((res) => res.data)
