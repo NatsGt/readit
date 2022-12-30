2e74a1b22e7f302012103fa1f1a90c8da7a3bbb9
natsgt <nataliarodas7858@gmail.com>
2022-12-30 11:43:03 +0100

    ass isloading

--- components/Search/SearchResults.tsx
+++ components/Search/SearchResults.tsx
@@ -11,11 +11,12 @@ interface QueryResult {
 }
 
 const SearchResults: React.FC<SearchResultsProps> = ({ query }) => {
-  const { data } = useGetBooks(query)
+  const { data, isLoading } = useGetBooks(query)
   console.log(data)
 
   return (
     <div>
+      {isLoading && <div>loading</div>}
       {data?.items?.map((results: QueryResult, index: number) => {
         return <div key={index}>{results.volumeInfo.title}</div>
       })}
