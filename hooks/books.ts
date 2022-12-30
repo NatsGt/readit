decbaf583cd6ed6da1b910299e804429f64b8c45
natsgt <nataliarodas7858@gmail.com>
2022-12-30 11:42:25 +0100

    add types to query response

--- hooks/books.ts
+++ hooks/books.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { GoogleBooks } from '../types/googleBooks'
 
 export const useGetBooks = (query: string) => {
-  return useQuery(['books', query], () =>
+  return useQuery<GoogleBooks>(['books', query], () =>
     axios
       .get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
       .then((res) => res.data)
